f6dbaf41a191b96f2738fb780d33aeb2f62e4dda
seongmin kim <k2417000@naver.com>
2024-07-22 18:05:53 +0900

    UserDefaultsManager 생성

--- ILikePhoto/UserDefaultsManager.swift
+++ ILikePhoto/UserDefaultsManager.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+enum UserDefaultsManager {
+    
+    enum Key: String, CaseIterable {
+        case nickname
+        case profileImageIndex
+        case mbti
+        case signUpDate
+    }
+    
+    @UserDefault(key: Key.nickname.rawValue, defaultValue: "")
+    static var nickname: String?
+    
+    @UserDefault(key: Key.profileImageIndex.rawValue, defaultValue: 0)
+    static var profileImageIndex: Int
+    
+    @UserDefault(key: Key.mbti.rawValue, defaultValue: "")
+    static var mbti: String
+    
+    @UserDefault(key: Key.signUpDate.rawValue, defaultValue: nil)
+    static var signUpDate: Date?
+    
+    static func removeAll() {
+        UserDefaults.standard.dictionaryRepresentation().keys.forEach {
+            UserDefaults.standard.removeObject(forKey: $0)
+        }
+    }
+}
