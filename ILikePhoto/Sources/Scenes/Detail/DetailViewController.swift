1d56a83c6dc085a22c8ff18514c18fac9ef55ba0
seongmin kim <k2417000@naver.com>
2024-07-26 13:49:25 +0900

    사진 상세 화면 생성

---
+++ ILikePhoto/Sources/Scenes/Detail/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  ILikePhoto
+//
+//  Created by 김성민 on 7/26/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class DetailViewController: BaseViewController {
+    
+}
---
+++ ILikePhoto/Sources/Scenes/Detail/DetailViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewModel.swift
+//  ILikePhoto
+//
+//  Created by 김성민 on 7/26/24.
+//
+
+import Foundation
+
+final class DetailViewModel: BaseViewModel {
+    
+    // Input
+    
+    
+    // Output
+    
+    
+    override func transform() {
+        //
+    }
+}
